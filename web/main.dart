d2ce9e05994a30396314990548f05c9dc74b829d
Cesar Acosta <cesar.acosta@gmail.com>
2018-05-09 23:07:52 -0400

    App Web Base - Firestore Gmaps Dart Funcionando

--- web/main.dart
+++ web/main.dart
@@ -5,38 +5,41 @@ import 'package:firebase/firestore.dart' as fs;
 import 'package:google_maps/google_maps.dart';
 //import 'package:firebase/src/assets/assets.dart';
 
+GMap googleMap;
 
 void main() {
- /* fb.initializeApp(
+ fb.initializeApp(
     apiKey: "AIzaSyDLcSXnz9AWjfCixVxgHqW7fBVdgiRvzH4",
     authDomain: "misasrd-39bbd.firebaseapp.com",
     databaseURL: "https://misasrd-39bbd.firebaseio.com",
     projectId: "misasrd-39bbd",
     storageBucket: "misasrd-39bbd.appspot.com",
     messagingSenderId: "343153881263"
-    );*/
+    );
 
-  //_LoadMap();
-/*
-  //fs.Firestore firestore = firestore();
-  fs.CollectionReference ref = fb.firestore().collection("iglesias");
+print("inicial el mapa");
+
+  _LoadMap();
+
+print("Final el mapa");
+
+
+fs.CollectionReference ref = fb.firestore().collection("iglesias");
 
 var ulista = querySelector("#liparroquia");
 
 
 print(ref.id);
 
-var map;
-
 ref.get().then((querySnapshot) {
     querySnapshot.forEach((doc) {
         // doc.data() is never undefined for query doc snapshots
     var spanElement = new SpanElement()..text = doc.data()["nombre"];
-    spanElement.appendText("-");
+    spanElement.appendText(" - ");
     spanElement.appendText(doc.data()["sector"]);
 
     var aElementLocate = new AnchorElement(href: "#")
-      ..text = "Localizar"
+      ..text = " Localizar"
       ..onClick.listen((e) {
         e.preventDefault();
         _ShowOnMap(doc.data()["latlong"]);
@@ -51,24 +54,15 @@ ref.get().then((querySnapshot) {
     });
 });
 
-print(map);
-
-print("hola");*/
-
-print("inicial el mapa");
-  final mapOptions = new MapOptions()
-      ..zoom = 8
-      ..center = new LatLng(-34.397, 150.644);
-    new GMap(document.getElementById('map-canvas'), mapOptions);
-  
-    print("ya acabo del mapa");
-
-  querySelector('#output').text = 'Your Dart app is running.';
+querySelector('#output').text = 'Your Dart app is running.';
 }
 
 _LoadMap(){
- 
-
+ final mapOptions = new MapOptions()
+      ..zoom = 8
+      ..center = new LatLng(-34.397, 150.644);
+  googleMap=  new GMap(document.getElementById('map-canvas'), mapOptions);
+  
 }
 
 
@@ -85,5 +79,18 @@ for (var key in latlong.keys){
 print(latlong);
 print(lat);
 print(long);
+
+_PlaceMarker(lat,long);
   
+}
+
+_PlaceMarker(lat,long){
+
+  googleMap.center= new LatLng(lat, long);
+  googleMap.zoom=15;
+
+final marker = new Marker(new MarkerOptions()
+    ..map = googleMap
+..position = googleMap.center);
+
 }
